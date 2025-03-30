5ba19e10f35decbf9f293a7734d72ca89e12591f
hanabi <mwx0621@gmail.com>
2025-03-30 16:17:56 +0800

    feat: 🎸 simple support for custom icon

--- talos/src/data/marker/index.js
+++ talos/src/data/marker/index.js
@@ -9,8 +9,9 @@ import subregion6 from './data/subregion-6.json'
 import dijiang from './data/Dijiang.json'
 import jinlong from './data/Jinlong.json'
 import markerTypeDict from './type.json'
-import { icon } from 'leaflet'
+import { divIcon, icon } from 'leaflet'
 import LOGGER from '../../utils/log'
+import "./index.scss"
 
 export const SUBREGION_MARKS_MAP = {
     'subregion-1': subregion1,
@@ -38,11 +39,13 @@ export const MAEKER_TYPE_TREE = Object.values(MARKER_TYPE_DICT).reduce((acc, typ
 */
 export const MARKER_TYPE_ICON_DICT = Object.values(MARKER_TYPE_DICT).reduce((acc, type) => {
     const iconUrl = ["tp", "hub"].includes(type.key) ? getMarkerIconUrl(type.key) : getMarkerIconUrl("default")
-    acc[type.key] = icon({
+    acc[type.key] = divIcon({
         iconUrl,
-        iconSize: [50, 50],
+        // iconSize: [50, 50],
         iconAnchor: [25, 25],
-        popupAnchor: [0, 0],
+        // popupAnchor: [0, 0],
+        className: "custom-marker-icon",
+        html: `</div><div class="custom-marker-icon-border"></div><div class="custom-marker-icon-bg" style="background-image: url(${iconUrl})">`
     })
     return acc
 }
