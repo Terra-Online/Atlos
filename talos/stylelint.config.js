571c6d1cc692e3076518592ccf44dfdebd29b3dc
502y <53784463+502y@users.noreply.github.com>
2025-09-21 20:43:04 +0800

    chore: update stylelint

--- talos/stylelint.config.js
+++ talos/stylelint.config.js
@@ -2,6 +2,27 @@
 export default {
     extends: ['stylelint-config-standard', 'stylelint-config-standard-scss'],
     rules: {
-        'selector-class-pattern': null,
+        // 空行
+        'at-rule-empty-line-before': null,
+        'comment-empty-line-before': null,
+        'custom-property-empty-line-before': null,
+        'declaration-empty-line-before': null,
+        'rule-empty-line-before': null,
+        // 颜色
+        'color-function-notation': null,
+        'alpha-value-notation': null,
+        // 变量名
+        'selector-class-pattern': [
+            '^[a-z]+([A-Z][a-z0-9]*)*$',
+            {
+                message: 'Expected class selector to be in camelCase',
+            },
+        ],
+        'keyframes-name-pattern': [
+            '^[a-z]+([A-Z][a-z0-9]*)*$',
+            {
+                message: 'Expected keyframe name to be in camelCase',
+            },
+        ],
     },
 };
