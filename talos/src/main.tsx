e49d7f325c977e3c772eff356d169ecb9f979340
cirisus <cirisus@outlook.com>
2025-11-09 14:55:52 +0800

    upd: hide LazyApp and use App directly

--- talos/src/main.tsx
+++ talos/src/main.tsx
@@ -1,5 +1,14 @@
 import { createRoot } from 'react-dom/client';
-import LazyApp from '@/LazyApp.tsx';
+//import LazyApp from '@/LazyApp.tsx';
+import App from './App.tsx';
+import { fontLoader } from './utils/fontLoader.ts';
+
+fontLoader();
 
 // @ts-expect-error root must be found otherwise it will definitely cannot show anything
-createRoot(document.getElementById('root')).render(<LazyApp />);
+createRoot(document.getElementById('root')).render(<App />);
+
+/*
+ **Lazyapp now temporarily disabled due to actually it's unnecessary for current resource scale.
+    createRoot(document.getElementById('root')).render(<LazyApp />);
+*/
